4fd9354aebba8303eaebbdc4ac038a9cbe9a10d0
Hannes <reihe0@snthetik.com>
2018-10-09 13:39:55 +0200

    started encryption

--- app/src/main/java/com/snthetik/exchanger/MainActivity.java
+++ app/src/main/java/com/snthetik/exchanger/MainActivity.java
@@ -1,21 +1,50 @@
 package com.snthetik.exchanger;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.snthetik.exchanger.MESSAGE";
     public static final String EXTRA_URL = "com.snthetik.exchanger.URL";
     public static final String EXTRA_PASS = "com.snthetik.exchanger.PASS";
     public static boolean connected=false;
+    static boolean useEncryption=false;
+    static KeyPair keyPair;
+    static PublicKey pubKey;
+    static PrivateKey privKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(Build.VERSION.SDK_INT>=26){
+            useEncryption=true;
+            System.out.println("Trying save mode");
+            try {
+                keyPair = buildKeyPair();
+                pubKey = keyPair.getPublic();
+                privKey = keyPair.getPrivate();
+            }catch(Exception e){
+                e.printStackTrace();
+                }
+        }else{
+            useEncryption=false;
+            System.out.println("Insecure mode");
+        }
     }
 
     /** Called when the user taps the Send button */
@@ -63,6 +92,36 @@ public class MainActivity extends AppCompatActivity {
             connected=false;
         }
         super.onDestroy();
+    }
+
+    public static KeyPair buildKeyPair() throws NoSuchAlgorithmException {
+        final int keySize = 2048;
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(keySize);
+        return keyPairGenerator.genKeyPair();
+    }
+
+    public static byte[] encrypt(PublicKey publicKey, String message) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+        return cipher.doFinal(message.getBytes());
+    }
+
+    public static byte[] decrypt(PrivateKey privateKey, byte [] encrypted) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+
+        return cipher.doFinal(encrypted);
+    }
+
+    @TargetApi(26)
+    public static String publicKeyToString(PublicKey p) {
+
+        byte[] publicKeyBytes = p.getEncoded();
+
+        return Base64.getEncoder().encodeToString(publicKeyBytes);
+
     }
 
 }
--- app/src/main/java/com/snthetik/exchanger/NetworkHandler.java
+++ app/src/main/java/com/snthetik/exchanger/NetworkHandler.java
@@ -1,14 +1,21 @@
 package com.snthetik.exchanger;
 
+import android.annotation.TargetApi;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 
 import java.net.Socket;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 public class NetworkHandler extends Thread{
     public static final String EXTRA_MESSAGE_N = "com.snthetik.exchanger.MESSAGEN";
     public static boolean alreadylistening=false;
+    public static PublicKey serverKey;
     public DisplayMessageActivity whocalledme;
     public String mestoSend;
     public String pass;
@@ -25,13 +32,24 @@ public class NetworkHandler extends Thread{
             else System.out.println("nicht die erste nachricht");
             String answer = "";
             DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-            outToServer.write(new String(mestoSend + "\n").getBytes("UTF8"));
+            if(serverKey==null||!MainActivity.useEncryption) {
+                outToServer.write(new String(mestoSend + "\n").getBytes("UTF8"));
+            }else{
+
+                String enc ="#encoded#"+new String(MainActivity.encrypt(serverKey,mestoSend),"UTF8");
+                enc=enc.replaceAll("\n","#n#");
+                enc+="\n";
+                outToServer.write(enc.getBytes(("UTF8")));
+            }
 
             if(!alreadylistening){
                 alreadylistening=true;
                 stayConnected=true;
                 outToServer.write(new String(pass + "\n").getBytes("UTF8"));
-
+                if(MainActivity.useEncryption){
+                    String encKey = "#pubkey#" + MainActivity.publicKeyToString(MainActivity.pubKey) + "\n";
+                    outToServer.write(encKey.getBytes("UTF8"));
+                }
             }else{
 
                 return;
@@ -40,13 +58,26 @@ public class NetworkHandler extends Thread{
 
 
                 BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(),"UTF8"));
-                System.out.println(clientSocket.isConnected());
+                //System.out.println(clientSocket.isConnected());
                 //sentence = inFromUser.readLine();
 
 
                 answer = inFromServer.readLine();
                 System.out.println("antwort erhalten: " + answer);
                 if(answer==null){stayConnected=false;alreadylistening=false;}
+                if(answer.startsWith("#pubkey#")){
+                    answer=answer.replaceAll("#pubkey#","");
+                    answer=answer.replaceAll("\n","");
+                    decodeKey(answer);
+                    answer="Connection is secure!\n";
+                }else if(answer.startsWith("#encoded#")){
+                    if(serverKey!=null&&MainActivity.useEncryption) {			//decrypt here
+                        answer=answer.replaceAll("#encoded#", "");
+                        answer=answer.replaceAll("\n", "");
+                        answer=answer.replaceAll("#n#","\n");
+                        answer = new String(MainActivity.decrypt(MainActivity.privKey, answer.getBytes()));
+                    }
+                }
                 whocalledme.HeyImFinished(answer);
             }
 
@@ -63,5 +94,18 @@ public class NetworkHandler extends Thread{
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
         outToServer.write(new String(mes + "\n").getBytes("UTF8"));
         System.out.println("daten gesendet");
+    }
+
+    @TargetApi(26)
+    private void decodeKey(String answer){
+        try {
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+
+
+            X509EncodedKeySpec keySpecX509 = new X509EncodedKeySpec(Base64.getDecoder().decode(answer));
+            serverKey = kf.generatePublic(keySpecX509);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
